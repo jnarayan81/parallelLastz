205c7772c926c2ffaa8fc8610ce7a0e32624c573
Jitendra Narayan <jnarayan81@gmail.com>
2017-03-13 11:06:24 +0100

    Update parallelLastz.pl

--- parallelLastz.pl
+++ parallelLastz.pl
@@ -13,18 +13,22 @@ use Bio::SeqIO;
 #Usage: perl parallelLastz.pl <multi_fasta_qfile> <tfile> <thread>
 #Change the Lastz parameters at Line:81
 
-my ($qfile, $tfile, $thread, $debug, $help, $man);
+my ($qfile, $tfile, $config, $thread, $debug, $help, $man);
 my $version=0.1;
 GetOptions(
     'qfile|q=s' => \$qfile,
     'tfile|t=s' => \$tfile,
-    'core|c=i' => \$thread,
+    'cfile|c=s' => \$config,
+    'speedup|s=i' => \$thread,
     'help|h' => \$help
 ) or die &help($version);
 &help($version) if $help;
 #pod2usage("$0: \nI am afraid, no files given.")  if ((@ARGV == 0) && (-t STDIN));
-    
+
+if (!$qfile or !$tfile or !$config) { help($version) }
 if (!$thread) { $thread = `grep -c -P '^processor\\s+:' /proc/cpuinfo` }
+my $parameters = readConfig ($config);
+my $param = join (' ', @$parameters);
 
 my %sequences;
 my $seqio = Bio::SeqIO->new(-file => "$qfile", -format => "fasta");
@@ -42,8 +46,7 @@ while(my$seqobj = $seqio->next_seq) {
   # get it's exit code
   $pm->run_on_finish (
     sub { my ($pid, $exit_code, $ident) = @_;
-      #print "** $ident just got out of the pool ".
-        "with PID $pid and exit code: $exit_code\n";
+      #print "** $ident just got out of the pool ". "with PID $pid and exit code: $exit_code\n";
     }
   );
 
@@ -78,11 +81,41 @@ my $seq=$sequences{$name};
 	print $tmp_fh ">$name\n$seq\n";
 
         print "Working on $name sequence\n";
-	my $myLASTZ="lastz $tmp_fh $tfile --chain --output=seeALN_$name.lz --format=general- --progress --ambiguous=iupac --identity=90";
+	my $myLASTZ="lastz $tmp_fh $tfile --output=seeALN_$name.lz $param";
         system ("$myLASTZ");
 
 }
 
+#Read config files
+sub readConfig {
+my ($file) = @_;
+my $fh= read_fh($file);
+my @lines;
+while (<$fh>) {
+    chomp;
+    next if /^#/;
+    next if /^$/;
+    $_ =~ s/^\s+|\s+$//g;
+    push @lines, $_;
+}
+close $fh or die "Cannot close $file: $!";
+return \@lines;
+}
+
+#Open and Read a file
+sub read_fh {
+    my $filename = shift @_;
+    my $filehandle;
+    if ($filename =~ /gz$/) {
+        open $filehandle, "gunzip -dc $filename |" or die $!;
+    }
+    else {
+        open $filehandle, "<$filename" or die $!;
+    }
+    return $filehandle;
+}
+
+#Help section
 sub help {
   my $ver = $_[0];
   print "\n parallelLastz $ver\n\n";
@@ -91,7 +124,8 @@ sub help {
   print	"Options:\n";
   print "	--qfile|-q	query multifasta/fasta file\n";
   print "	--tfile|-t	target genome file\n";
-  print "	--core|-c	number of core to use\n";
+  print "	--cfile|-c	config file\n";
+  print "	--sppedup|-s	number of core to use\n";
   print "     	--help|-h	brief help message\n";
 
 exit;
