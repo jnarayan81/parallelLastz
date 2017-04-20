1dfcea4ba52182b7e178fd3468e04defc82fda82
Jitendra Narayan <jnarayan81@gmail.com>
2017-04-20 16:56:47 +0200

    Update parallelLastz.pl

--- parallelLastz.pl
+++ parallelLastz.pl
@@ -10,7 +10,7 @@ use Parallel::ForkManager;
 use Bio::SeqIO;
 
 #Author: Jitendra Narayan
-#Usage: perl parallelLastz.pl <multi_fasta_qfile> <tfile> <cfile> <thread>
+#Usage: perl parallelLastz.pl <multi_fasta_qfile> <tfile> <cfile> <thread> <length>
 #Change the Lastz parameters at Line:84
 
 my ($qfile, $tfile, $config, $thread, $debug, $help, $man, $length);
